2e281f5805a8e2994bceca7c64af0fabaee75364
Felipe Santos <rfelipe273@gmail.com>
2024-05-30 18:28:44 -0300

    refactor: PlaceAnOrderCommand is now a class.

--- RamenStore.Application/Commands/Orders/PlaceAnOrder/PlaceAnOrderCommand.cs
+++ RamenStore.Application/Commands/Orders/PlaceAnOrder/PlaceAnOrderCommand.cs
@@ -1,5 +1,12 @@
 ﻿using RamenStore.Application.Abstractions.Messaging;
+using System.Text.Json.Serialization;
 
 namespace RamenStore.Application.Commands.Orders.PlaceAnOrder;
 
-public sealed record PlaceAnOrderCommand(string BrothId, string ProteinId) : ICommand<PlaceAnOrderCommandResponse>;
+public sealed class PlaceAnOrderCommand : ICommand<PlaceAnOrderCommandResponse>
+{
+    [JsonPropertyName("brothId")]
+    public string BrothId { get; set; }
+    [JsonPropertyName("proteinId")]
+    public string ProteinId { get; set; }
+}
